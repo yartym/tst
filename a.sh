511d52d97be3dca53971754f9cda761c60a1d04a
yartym <yartym@gmail.com>
2016-03-16 17:04:27 +0300

    Added files via upload

--- a.sh
+++ a.sh
@@ -1,1 +1,8 @@
-mkdir /home/box/web/new
+mkdir -p /home/box/web/public/css
+mkdir -p /home/box/web/public/img
+mkdir -p /home/box/web/public/js
+mkdir -p /home/box/web/etc
+mkdir -p /home/box/web/uploads
+
+
+
